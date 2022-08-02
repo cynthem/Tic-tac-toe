2e670040da5d2d476a9ceff6a6f6efa6a7016455
doozles411 <cindy.phelps01@gmail.com>
2022-08-02 16:03:40 -0700

    Adjust click event animations and styles

--- scripts.js
+++ scripts.js
@@ -1,14 +1,23 @@
 const selectPlayer = (() => {
     const humanPlayer = document.querySelector('.fa-person');
     const computerPlayer = document.querySelector('.fa-display');
-    const choiceCard = document.querySelector('.choose-player');
+    const choiceScreen = document.querySelector('.choose-player');
+    const gameScreen = document.querySelector('.main-game');
 
     humanPlayer.addEventListener('click', () => {
-        choiceCard.classList.remove('enter');
-        choiceCard.classList.add('exit');
-        window.setTimeout(() => choiceCard.classList.add('hide'), 1001);
-        //otherPlayer();
+        choiceScreen.classList.remove('enter');
+        choiceScreen.classList.add('exit');
+        window.setTimeout(() => choiceScreen.style.display = 'none', 1001);
+        window.setTimeout(() => gameScreen.style.display = 'grid', 1001);
+        window.setTimeout(() => gameScreen.classList.add('show'), 1001);
+        window.setTimeout(() => gameScreen.classList.remove('show'), 2002);
     })
-})
-
-selectPlayer();
+    computerPlayer.addEventListener('click', () => {
+        choiceScreen.classList.remove('enter');
+        choiceScreen.classList.add('exit');
+        window.setTimeout(() => choiceScreen.style.display = 'none', 1001);
+        window.setTimeout(() => gameScreen.style.display = 'grid', 1001);
+        window.setTimeout(() => gameScreen.classList.add('show'), 1001);
+        window.setTimeout(() => gameScreen.classList.remove('show'), 2002);
+    })
+})();
