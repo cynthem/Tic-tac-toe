178415de3dd623972851192a00942cbaf717bb32
doozles411 <cindy.phelps01@gmail.com>
2022-08-02 17:04:20 -0700

    Initialize gameboard and player functions

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@ const selectPlayer = (() => {
     const gameScreen = document.querySelector('.main-game');
 
     humanPlayer.addEventListener('click', () => {
+        gameBoard.setPlayer(1, 'player1');
+        gameBoard.setPlayer(2, 'player2');
         choiceScreen.classList.remove('enter');
         choiceScreen.classList.add('exit');
         window.setTimeout(() => choiceScreen.style.display = 'none', 1001);
@@ -13,6 +15,8 @@ const selectPlayer = (() => {
         window.setTimeout(() => gameScreen.classList.remove('show'), 2002);
     })
     computerPlayer.addEventListener('click', () => {
+        gameBoard.setPlayer(1, 'player1');
+        gameBoard.setPlayer(2, 'computer');
         choiceScreen.classList.remove('enter');
         choiceScreen.classList.add('exit');
         window.setTimeout(() => choiceScreen.style.display = 'none', 1001);
@@ -21,3 +25,38 @@ const selectPlayer = (() => {
         window.setTimeout(() => gameScreen.classList.remove('show'), 2002);
     })
 })();
+
+const gameBoard = (() => {
+    const board = document.querySelector('.gameboard');
+    const playerOne = Player('X', false, 'player1');
+    const playerTwo = Player('O', false, 'player2');
+
+    setPlayer = (playerNumber, playerType) => {
+        return playerNumber === 1 ? playerOne.updateType(playerType) : playerTwo.updateType(playerType);
+    }
+})
+
+const Player = ((letter, currentlyPlaying, playerType) => {
+    const getLetter = () => {
+        return letter;
+    }
+    const getCurrentPlayer = () => {
+        return currentlyPlaying;
+    }
+    const updateCurrentPlayer = (isPlaying) => {
+        return currentlyPlaying = isPlaying;
+    }
+    const getType = () => {
+        return playerType;
+    }
+    const updateType = (type) => {
+        return playerType = type;
+    }
+    return {
+        getLetter,
+        getCurrentPlayer,
+        updateCurrentPlayer,
+        getType,
+        updateType
+    };
+})
